93917a9b2f58b9b314633d11fc65e40d05de3acb
bmac <bmac325@GMail.com>
2015-04-14 16:11:09 -0400

    Allow a user to get the content out of the config

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* jshint node: true */
 'use strict';
 
+function isObject(x) {
+  return typeof x === 'object' && x !== null;
+}
+
+function getProp(obj, path) {
+  if (!isObject(obj) || typeof path !== 'string') {
+    return obj;
+  }
+
+  path = path.split('.');
+
+  return getProp(obj[path.shift()], path.length && path.join('.'));
+};
+
 module.exports = {
-  name: 'ember-cli-content-for-config'
+  name: 'ember-cli-content-for-config',
+  contentFor: function(type, config) {
+    var path = type.split('.');
+    
+    if (path.shift() === 'config') {
+      return getProp(config, path.join('.'));
+    }
+  }
 };
